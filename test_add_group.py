c9c6f3b1423952736ba5ec7aa0f430f51a476e14
nekitvand <nekitvand@gmail.com>
2018-05-29 13:49:56 +0300

    Драйвер(2_1)
    Перенес вызовы методов  (self.open_home_page(wd),self.open_group_page(wd),self.return_to_groups_page(wd)) внутрь методов (login и create_group)
    
    Теперь тесты содержат только вызовы методов: self.login,self.create_group,self.logout

--- test_add_group.py
+++ test_add_group.py
@@ -14,7 +14,7 @@ class test_add_group(unittest.TestCase):
 
 
     def login(self, wd, username, password):
-        # login
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -30,6 +30,7 @@ class test_add_group(unittest.TestCase):
 
 
     def create_group(self, wd, group):
+        self.open_group_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -44,6 +45,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit to group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
 
     def return_to_groups_page(self, wd):
@@ -58,20 +60,14 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="aaaaa", header="aaaav", footer="aaaab"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def tearDown(self):
