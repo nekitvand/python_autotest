78505bcb36f85604dc1b6f1849f668b10154be30
nekitvand <nekitvand@gmail.com>
2018-05-29 12:42:57 +0300

    Выделение вспомогательных классов
    Создал новый .py файл - group.
    В нем создал класс Group.Внутри класса создал конструктор с атрибутами.В методе create_group передаваемые параметры (name,header,footer) переделал на group.name и т.д. Это нужно чтобы не передавать все параметры внутри  метода, а разграничить параметры ,создав отдельный файл и класс (Group).
    
    В методе add_group теперь атрибуты передаются через обращение к классу:   Group(name="aaaaa", header="aaaav", footer="aaaab")

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium.webdriver.firefox.webdriver import WebDriver
+from group import Group
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
@@ -28,19 +29,19 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit to group creation
         wd.find_element_by_name("submit").click()
 
@@ -60,7 +61,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="aaaaa", header="aaaav", footer="aaaab")
+        self.create_group(wd, Group(name="aaaaa", header="aaaav", footer="aaaab"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -69,7 +70,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
