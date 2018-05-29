e7b4519679a8b19a0179f7c3f44a475c5f1404a4
nekitvand <nekitvand@gmail.com>
2018-05-29 11:19:38 +0300

    Передача параметров в методы (1_5)
    Передал параметры в метод login (username,password) и в метод create group (name,header,footer).Параметры сделал не дефолтными.Значения этих параметров передают в вызове методов (self.login и self.create_group)

--- test_add_group.py
+++ test_add_group.py
@@ -12,14 +12,14 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
@@ -28,19 +28,19 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("aaaaa")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("aaaav")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("aaaab")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit to group creation
         wd.find_element_by_name("submit").click()
 
@@ -55,19 +55,23 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
 
 
-    def test_test_add_group(self):
-        success = True
+    def test_add_group(self):
         wd = self.wd
-        if __name__ == '__main__':
-            wd = self.driver
-
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="aaaaa", header="aaaav", footer="aaaab")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
        self.wd.quit()
