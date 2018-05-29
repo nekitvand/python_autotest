a92f69776b544c3a9c06aabea2ec36d2e170bb24
nekitvand <nekitvand@gmail.com>
2018-05-29 16:45:46 +0300

    Фикстура (2_2) - Часть 1(UnitTest)
    Удалил везде аргумент wd и использовать в каждом методе wd = self.wd
    Создал новый файл application с классом Application.Создал конструктор,в него перенес инициализацию драйвера и  ожидания.В этот файл перенес методы и создал метод def destroy куда переместил действия из def tear down.
    В файле test_add_group переписал метод setup на self.app = Application() и метод teardown теперь выглядит как self.app.destroy().Вызовы методов теперь идут через app (self.app.logout()).

--- test_add_group.py
+++ test_add_group.py
@@ -1,102 +1,31 @@
 import unittest
-from selenium.webdriver.firefox.webdriver import WebDriver
+from application import Application
 from group import Group
 from group import Group_add
 
+
 class test_add_group(unittest.TestCase):
     def setUp(self):
-      self.wd = WebDriver()
-      self.wd.implicitly_wait(3)
-
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
-
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-
-    def open_group_page(self, wd):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-
-
-    def create_group(self, wd, group):
-        self.open_group_page(wd)
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit to group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
-
-    def create_add_new(self, wd, group):
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_name("submit").click()
-
-
-    def return_to_home_page(self,wd):
-        wd.find_element_by_link_text("home page").click()
-
-
-    def return_to_groups_page(self, wd):
-        # return group page
-        wd.find_element_by_link_text("group page").click()
-
-
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
+     self.app = Application()
 
     def test_add_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="aaaaa", header="aaaav", footer="aaaab"))
-        self.logout(wd)
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="aaaaa", header="aaaav", footer="aaaab"))
+        self.app.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
+        self.app.login(username="admin", password="secret")
+        self.app.create_group(Group(name="", header="", footer=""))
+        self.app.logout()
 
     def test_add_address_book(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_add_new(wd ,Group_add('Nikita','Sergeeyvich','Vandyshev'))
-        self.logout(wd)
+        self.app.login(username="admin", password="secret")
+        self.app.create_add_new(Group_add(firstname='Nikita', middlename='Sergeeyvich', lastname='Vandyshev'))
+        self.app.logout()
 
 
     def tearDown(self):
-       self.wd.quit()
+       self.app.destroy()
 
 if __name__ == '__main__':
     unittest.main
