4bd573203caf9bcde35a126767a80e67b07e0efb
nekitvand <nekitvand@gmail.com>
2018-06-01 15:55:14 +0300

    Декомпозиция сложных классов (2_3) (Часть 2)
    Создал новый файл в fixture - group.В нем создал класс - GroupHelper.В него перенес все методы которые находились в application и предназначались для работы с группами.Создал контруктор для создания фиксуры.
    Создал импорт в application и прописал к вызову метода create group - app.group.create (проведя переименнование метода create group в group)

---
+++ fixture/group.py
@@ -0,0 +1,51 @@
+
+
+class GroupHelper:
+
+    def __init__(self,app):
+        self.app = app
+
+     def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def create_add_new(self, group):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("submit").click()
+
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,15 +13,15 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="aaaaa", header="aaaav", footer="aaaab"))
+    app.create(Group(name="aaaaa", header="aaaav", footer="aaaab"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
 def test_add_address_book(app):
     app.session.login(username="admin", password="secret")
-    app.create_add_new(Group_add(firstname='Nikita', middlename='Sergeeyvich', lastname='Vandyshev'))
+    app.group.create_add_new(Group_add(firstname='Nikita', middlename='Sergeeyvich', lastname='Vandyshev'))
     app.session.logout()
