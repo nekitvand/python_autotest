4bb1758f035e7f0e93433a24f2db9f2df3edda7d
nekitvand <nekitvand@gmail.com>
2018-07-01 00:43:43 +0300

    Тесты на модификацию (3_1)
    Создал новый файл (test_modify_group),который модифицирует Header и Name group (test_modify_group и test_modify_header)  Создал новые методы - fill_group_form , change_field_value, select_first_group , modify_first_group

--- fixture/group.py
+++ fixture/group.py
@@ -14,17 +14,22 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def create_add_new(self, group):
         wd = self.app.wd
@@ -49,8 +54,20 @@ class GroupHelper:
     def delete_firts_group(self):
         wd = self.app.wd
         self.open_group_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group(wd)
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def select_first_group(self, wd):
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self,new_group_data):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_first_group(wd)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
 
