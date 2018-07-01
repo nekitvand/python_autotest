ade5ed84f2d354b9c7b9627965848c526d63db66
nekitvand <nekitvand@gmail.com>
2018-07-02 00:24:36 +0300

    Интелектуальная фикстура (3_2)
    Доработал фикстуру,чтобы при разрушении фикстуры по каким-либо (закрылось окно,крашнулся браузер),фикстура производила проверку на валидность и\или создавала новую фикстуру.Так же добавил scope="session" и  autouse=True.чтобы фикстура не распадалась после каждого\и не указывалась явно в тестах.
    Создан новый метод is_value(для проверки пригодности фикстуры.Он возращает адресс url)

---
+++ conftest.py
@@ -0,0 +1,27 @@
+
+import pytest
+from fixture.application import Application
+
+
+fixture = None
+
+@pytest.fixture
+def app(request):
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,16 +3,10 @@ from model.group import Group
 from model.group import Group_add
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="aaaaa", header="aaaav", footer="aaaab"))
-    app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
 
 def test_add_address_book(app):
-    app.session.login(username="admin", password="secret")
     app.group.create_add_new(Group_add(firstname='Nikita', middlename='Sergeeyvich', lastname='Vandyshev'))
-    app.session.logout()
