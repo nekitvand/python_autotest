48564f87d889ec2fbe8184755145172dc1a7670e
nekitvand <nekitvand@gmail.com>
2018-07-02 15:10:27 +0300

    Интелектуальный логин (3_3)
    Создал предусловия проверки входа\выхода в систему.
    Даже если система выйдет (совершит logout) , то произойдет проверка и система снова выполнит авторизацию.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,7 @@ from model.group import Group_add
 def test_add_group(app):
     app.group.create(Group(name="aaaaa", header="aaaav", footer="aaaab"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
