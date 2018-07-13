a5de02f137696fd78234d9a5529b9df600feb290
nekitvand <nekitvand@gmail.com>
2018-07-13 12:54:48 +0300

    Кеширование и хеширование(4_4)
    Добавил новую переменную для хранения кеша (group_cache = None).
    Создал логику для кеширования и хеширования тестов

--- fixture/group.py
+++ fixture/group.py
@@ -18,6 +18,7 @@ class GroupHelper:
         self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -58,6 +59,7 @@ class GroupHelper:
         self.select_first_group(wd)
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self, wd):
         wd.find_element_by_name("selected[]").click()
@@ -70,6 +72,7 @@ class GroupHelper:
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
 
     def return_to_groups_page(self):
@@ -82,12 +85,16 @@ class GroupHelper:
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups =[]
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text , id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache =[]
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text , id=id))
+        return list(self.group_cache)
