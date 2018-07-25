e5c3e474708c921a94d757171af266d9542e743e
Nikita Vandyshev <n.vandyshev@bestdoctor.ru>
2018-07-25 16:55:37 +0300

    Режим строки.Обьединение строк(5_2) Часть 1
    Созданы 2 файла contact (model,fixture).Созданы внутри них методы и конструкторы.

---
+++ fixture/contact.py
@@ -0,0 +1,46 @@
+
+
+from model.contact import Contact
+
+
+class ContactHelper:
+
+    def __init__(self):
+        self.app = app
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    contact_cache = None
+
+    def get_contact_list(self):
+        if self.contact_cache is None
+            wd = self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            for row in wd.find_element_by_name["entry"]:
+                cells = row.find_elements_by_tag_name("td")
+                firstname = cells[1].text
+                lastname = cells[2].text
+                id = cells[0].find_elements_by_tag_name("input").get_attribute('value')
+                self.contact_cache.append(Contact(firstname=firstname,lastname=lastname,id=id))
+        return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_element_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_element_by_name("entry")
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
