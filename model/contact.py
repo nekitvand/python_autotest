ec0fe7f54265a76be52764feced7a0e7917c44da
Nikita <nekitvand@gmail.com>
2018-08-02 19:45:06 +0300

    Метод обратных проверок (5_3)
    Создал метод который собирает данные , в список а далее уже их склеивает , с очищением от пустых строк и значений

--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,8 @@ from sys import maxsize
 class Contact:
 
 
-    def __init__(self,firstname=None,lastname=None,homephone=None,mobilephone=None,workphone=None,secondaryphone=None,id=None):
+    def __init__(self,firstname=None,lastname=None,id=None,
+                 homephone=None,mobilephone=None,workphone=None,secondaryphone=None,all_phones_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.homephone = homephone
@@ -13,6 +14,7 @@ class Contact:
         self.workphone = workphone
         self.secondaryphone = secondaryphone
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname,self.lastname)
