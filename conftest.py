b07525a6bfc665e7a46c3d17e5fb642d6d9c406f
Nikita <nekitvand@gmail.com>
2018-08-04 23:30:00 +0300

    Загрузка тестовых данных из файла (6_5)
    Создал "фабрику тестов"

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 import pytest
 import json
 import os.path
+import importlib
 from fixture.application import Application
 
 
@@ -34,3 +35,12 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser",action="store",default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture,testdata,ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
