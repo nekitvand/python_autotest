0450cc9ede8592be094b12bea6163a76d24d6bd3
Nikita Vandyshev <n.vandyshev@bestdoctor.ru>
2018-08-06 18:07:01 +0300

    Загрузка тестовых данных из JSON (6_6)
    Создал коснтрукции для обратного преобразования и чтения JSON

--- generator/group.py
+++ generator/group.py
@@ -1,8 +1,10 @@
+import jsonpickle
+
 from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -14,7 +16,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 n = 5
-f = "data/group.json"
+f = "data/groups.json"
 
 for o, a in opts:
     if o == "-n":
@@ -37,4 +39,6 @@ testdata= [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file ,"w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json" , indent = 2)
+    out.write(jsonpickle.encode(testdata))
+   # out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
