9f8391abba15ed7c22240f2331272a91017cbb5d
Nikita Vandyshev <n.vandyshev@bestdoctor.ru>
2018-08-10 13:37:43 +0300

    Ограничения или условия выбора (7_6)
    Создал метод для получения листа контактов и создал запрос с условием where

--- fixture/newdb.py
+++ fixture/newdb.py
@@ -1,6 +1,7 @@
 
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
 
 class DbFixture:
 
@@ -9,9 +10,8 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = mysql.connector.connect(host=host, database =name, user=user,password = password)
+        self.connection = mysql.connector.connect(host=host, database=name, user=user,password=password)
         self.connection.autocommit = True
-
 
     def get_group_list(self):
         list = []
@@ -19,8 +19,8 @@ class DbFixture:
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
-                (id, name , header, footer) = row
-                list.append(Group(id=str(id),name=name,header=header,footer=footer))
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
         return list
@@ -28,3 +28,16 @@ class DbFixture:
 
     def destroy(self):
         self.connection.close()
+
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname , lastname from addressbook where deprecated='0000-00-00 00:00:0'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
